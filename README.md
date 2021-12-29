6441a45e57a9f124ba207f70a9931b4697f3cef9
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2021-12-28 21:45:00 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Phaser MMORPG User Authentication with MongoDB
+This is an API that allows new players to signup for our MMORPG game. A player can also verify that the API is up using the check status endpoint which will
+return a 200 if the route is still up and running. We will be using token authentication to keep a persistant login and logout feature that allows a player 
+to stay logged in without the need to login everytime.
+
+# Routes:
+    - 404 handler
+    - 500 handerl
+    - status
+    - signup
+    - login
+    - logout
+    - token 
+    - forgot password
+    - reset password
---
+++ index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+// Package imports
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require("cors");
+
+const routes = require("./routes/main");
+const passwordRoutes = require("./routes/password");
+
+// Initialize express object
+const app = express();
+
+// Create variables
+const port = process.env.PORT || 3000; // set the localhost port
+
+// update express settings
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(cors({ credentials: true, origin: process.env.CORS_ORIGIN }))
+
+// setup routes
+app.use("/", routes);
+app.use("/", passwordRoutes);
+
+// catch all other routes
+app.use((req, res) => {
+    res.status(404).json({ message: "404 - Not Found", status: 404 });
+});
+
+// handle errors
+app.use((error, req, res, next) => {
+    console.log(error);
+    res.status(error.status || 500).json({ error: error.message, status: 500});
+});
+
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`);
+});
