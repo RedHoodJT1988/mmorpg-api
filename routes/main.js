0e6e6a2b5f69d22586b617deffc2d4aaa8be47e8
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2021-12-29 12:57:21 -0800

    update: remove unnecessary code

--- routes/main.js
+++ routes/main.js
@@ -31,12 +31,6 @@ router.post("/login", (req, res, next) => {
             return next(err);
         }
     }) (req, res, next);
-
-    if (!req.body) {
-        res.status(400).json({ message: 'invalid body', status: 400 });
-    } else {
-        res.status(200).json({ message: "OK", status: 200 });
-    }
 });
 
 router.post("/logout", (req, res) => {
